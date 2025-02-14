15e851b7b677777652fb795389d29e2e02cbea96
JosiasmDev <josiasdicampus@gmail.com>
2025-02-14 10:41:28 +0100

    14.02 10.41

---
+++ apps/app_gestion_Funciona registro y login/app/models/usuario_model.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from app import db  # Importamos db desde la aplicación inicializada
+
+class Usuario(UserMixin, db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
