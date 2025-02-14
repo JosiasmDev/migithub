297d958b43152a214d13f9cc6ef48524b9c254e4
JosiasmDev <josiasdicampus@gmail.com>
2025-02-14 08:57:07 +0100

    as

--- app_gestion_4.0/app/__init__.py
+++ app_gestion_4.0/app/__init__.py
@@ -1,12 +1,17 @@
 # app/__init__.py - Inicialización de la aplicación
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config")
-    
-    # Registro de blueprints
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
     from app.controllers.usuario_controller import usuario_bp
     app.register_blueprint(usuario_bp)
-    
+
     return app
